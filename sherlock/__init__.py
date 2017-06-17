d7562390cef98f35d309e9f2f37d8cf88c31b711
Aleksandar Varga <aleksandar.varga@uns.ac.rs>
2017-06-17 17:06:51 +0200

    Postavljena genericka konekcija prema bazi.

--- sherlock/__init__.py
+++ sherlock/__init__.py
@@ -9,8 +9,7 @@ from sherlock.api.views import mod as api_mod
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aleksandar:ftn@localhost/sherlock'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/sherlock'
 
 app.register_blueprint(site_mod)
 app.register_blueprint(api_mod, url_prefix='/api')
